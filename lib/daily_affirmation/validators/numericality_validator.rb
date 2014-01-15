d1878e8639e1fc6321bccf75913bad63399bcfd2
Shane Emmons <shane@emmons.io>
2014-01-15 08:02:06 -0800

    allow numeric to check greater than and less than

--- lib/daily_affirmation/validators/numericality_validator.rb
+++ lib/daily_affirmation/validators/numericality_validator.rb
@@ -4,13 +4,60 @@ module DailyAffirmation
   module Validators
     class NumericalityValidator < Validator
       def valid?
-        @valid ||= value.is_a?(Numeric)
+        @valid ||= numeric? && greater_than? && less_than?
       end
 
       def error_message
         @error_message ||= i18n_error_message(
-          :numericality, :default => "#{attribute} is not a number"
+          :numericality, :default => default_error_message
         )
+      end
+
+      private
+
+      def numeric?
+        value.is_a?(Numeric)
+      end
+
+      def greater_than?
+        if gt = opts[:greater_than]
+          value > gt
+        else
+          true
+        end
+      end
+
+      def less_than?
+        if lt = opts[:less_than]
+          value < lt
+        else
+          true
+        end
+      end
+
+      def default_error_message
+        numeric_error_message + greater_than_error_message +
+          less_than_error_message
+      end
+
+      def numeric_error_message
+        "#{attribute} must be a number."
+      end
+
+      def greater_than_error_message
+        if greater_than?
+          ""
+        else
+          " Must also be greater than #{opts[:greater_than]}."
+        end
+      end
+
+      def less_than_error_message
+        if less_than?
+          ""
+        else
+          " Must also be less than #{opts[:less_than]}."
+        end
       end
     end
   end
--- spec/validators/numericality_validator_spec.rb
+++ spec/validators/numericality_validator_spec.rb
@@ -23,7 +23,57 @@ describe "NumericalityValidator" do
 
     validator = subject.new(obj, :age)
     expect(validator.error_message).to eq(
-      "age is not a number"
+      "age must be a number."
     )
+  end
+
+  context ":greater_than provided" do
+    it "passes validation if value is > opt" do
+      obj = double(:age => 10)
+
+      validator = subject.new(obj, :age, :greater_than => 5)
+      expect(validator).to be_valid
+    end
+
+    it "fails validation if value is < opt" do
+      obj = double(:age => 10)
+
+      validator = subject.new(obj, :age, :greater_than => 15)
+      expect(validator).to_not be_valid
+    end
+
+    it "has the correct error message" do
+      obj = double(:age => 10)
+
+      validator = subject.new(obj, :age, :greater_than => 15)
+      expect(validator.error_message).to eq(
+        "age must be a number. Must also be greater than 15."
+      )
+    end
+  end
+
+  context ":less_than provided" do
+    it "passes validation if value is < opt" do
+      obj = double(:age => 10)
+
+      validator = subject.new(obj, :age, :less_than => 15)
+      expect(validator).to be_valid
+    end
+
+    it "fails validation if value is > opt" do
+      obj = double(:age => 10)
+
+      validator = subject.new(obj, :age, :less_than => 5)
+      expect(validator).to_not be_valid
+    end
+
+    it "has the correct error message" do
+      obj = double(:age => 10)
+
+      validator = subject.new(obj, :age, :less_than => 5)
+      expect(validator.error_message).to eq(
+        "age must be a number. Must also be less than 5."
+      )
+    end
   end
 end
