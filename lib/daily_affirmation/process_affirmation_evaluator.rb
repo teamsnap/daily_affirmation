6443110cc65167368fef37031f2b0f44c5a3319b
Tyler Knappe <tyler.knappe@teamsnap.com>
2015-04-01 16:06:44 +0000

    Cleanup allow_blank and allow_nil methods, make it much easier to follow.

--- lib/daily_affirmation/process_affirmation_evaluator.rb
+++ lib/daily_affirmation/process_affirmation_evaluator.rb
@@ -6,7 +6,7 @@ class ProcessAffirmationEvaluator
   end
 
   def process?
-    if_statement_passes? && allow_nil_passes? && allow_blank_passes?
+    if_statement_passes? && process_non_nil_value? && process_non_blank_value?
   end
 
   private
@@ -21,6 +21,18 @@ class ProcessAffirmationEvaluator
     end
   end
 
+  def process_non_blank_value?
+    !(allow_blank_values? && object_is_blank?)
+  end
+
+  def allow_blank_values?
+    args.include?(:allow_blank)
+  end
+
+  def object_is_blank?
+    blank?(object.send(attribute))
+  end
+
   def blank?(val)
     case val
     when String
@@ -30,23 +42,15 @@ class ProcessAffirmationEvaluator
     end
   end
 
-  def allow_blank_passes?
-    if args.include?(:allow_blank)
-      if blank?(object.send(attribute))
-        false
-      else
-        true
-      end
-    else
-      true
-    end
+  def object_is_nil?
+    object.send(attribute).nil?
   end
 
-  def allow_nil_passes?
-    if args.include?(:allow_nil)
-      object.send(attribute)
-    else
-      true
-    end
+  def allow_nil_values?
+    args.include?(:allow_nil)
+  end
+
+  def process_non_nil_value?
+    !(allow_nil_values? && object_is_nil?)
   end
 end
